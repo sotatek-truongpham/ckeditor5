edbdb8df1a9dd63d5d53b2a5088fc0d02e2e9f76
sotatek-truongpham <truong.pham@sotatek.com>
2022-03-24 16:35:45 +0700

    Init

---
+++ build/translations/gu.js
@@ -0,0 +1,1 @@
+!function(i){const n=i.gu=i.gu||{};n.dictionary=Object.assign(n.dictionary||{},{Bold:"ઘાટુ - બોલ્ડ્",Code:"",Italic:"ત્રાંસુ - ઇટલિક્",Strikethrough:""}),n.getPluralForm=function(i){return 1!=i}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ src/ckeditor.js
@@ -0,0 +1,60 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
+
+class Editor extends ClassicEditor { }
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Bold,
+	Code,
+	Heading,
+	Italic,
+	Link,
+	List,
+	Paragraph,
+	Strikethrough,
+	CodeBlock
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'code',
+			'strikethrough',
+			'codeblock'
+		]
+	},
+	heading: {
+		options: [
+			{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+			{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+			{ model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+			{ model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' },
+			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' }
+		]
+	},
+	language: 'en'
+};
+
+export default Editor;
